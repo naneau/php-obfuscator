8e881220a8c15bfc69a1c0adb3ece94f015d980f
Vyacheslav Gulyam <gv@roistat.com>
2017-03-06 23:42:43 +0300

    test

--- tests/Base.php
+++ tests/Base.php
@@ -3,5 +3,5 @@
 namespace Tests;
 
 class Base extends \PHPUnit_Framework_TestCase {
-
+    
 }
--- tests/ObfuscateTest.php
+++ tests/ObfuscateTest.php
@@ -6,22 +6,53 @@ use PhpParser\Parser;
 use PhpParser\Lexer;
 use PhpParser\NodeTraverser;
 use PhpParser\PrettyPrinter\Standard as PrettyPrinter;
+use Symfony;
+use Naneau;
+use Symfony\Component\Console\Tests;
 
 class ObfuscateTest extends Base {
 
-    public function testObfuscate() {
-        $source = '<?php
-            class a {
-                private $vasya = "Pupkin";
-                private function test() {
-                echo "hi";
-            }
-            }';
-        $obfuscator = new Mock\Obfuscator();
-        $obfuscator->setParser(new Parser(new Lexer()));
-        $obfuscator->setTraverser(new NodeTraverser());
-        $obfuscator->setPrettyPrinter(new PrettyPrinter());
-        echo $obfuscator->obfuscateSource($source);
+    protected function setUp() {
+        mkdir("./tests/before");
     }
 
+    protected function tearDown() {
+        unlink("./tests/before/source.php");
+        unlink("./tests/after/source.php");
+        rmdir("./tests/before");
+        rmdir("./tests/after");
+    }
+
+    /**
+     * @param string $before
+     * @param string $after
+     * @dataProvider obfuscateProvider
+     */
+    public function testObfuscate($before, $after) {
+        file_put_contents("./tests/before/source.php", $before);
+        shell_exec("./bin/obfuscate obfuscate ./tests/before ./tests/after");
+        $obfuscated = file_get_contents("./tests/after/source.php");
+        $this->assertEquals($after, $obfuscated);
+    }
+
+    /**
+     * @return array
+     */
+    public function obfuscateProvider() {
+        return [
+            [
+'<?php
+
+class a {
+    private $vasya = "Pupkin";
+    private function test() {
+        echo "hi";
+    }
+}'
+,
+'<?php
+class a { private $vasya = \'Pupkin\'; private function spdfcc8c() { echo \'hi\'; } }'
+            ],
+        ];
+    }
 }
